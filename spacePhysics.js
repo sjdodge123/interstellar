8c1a81809b27d6772debf2f7d7a37e4acb585bf8
Jake Dodge <sjdodge@outlook.com>
2016-01-24 13:27:00 -0500

    extracted physics

---
+++ spacePhysics.js
@@ -0,0 +1,20 @@
+function updatePhysics(object) {
+	updateVelocity(object);
+	updatePosition(object);
+	updateRotation(object);
+	return object;
+}
+
+function updatePosition(object) {
+	object.x += object.velX;
+	object.y += object.velY;
+}
+
+function updateVelocity(object) {
+	object.velX = object.accelX;
+	object.velY = object.accelY;
+}
+
+function updateRotation(object) {
+
+}
