e1f06ef27576b32662e8c9cba80bfc0caa261911
rtoole13 <rtoole13@gmail.com>
2016-03-26 13:55:55 -0400

    utis/getRandomInt: Added function that returns a random int ranging from input min to max.

--- public/utils.js
+++ public/utils.js
@@ -68,6 +68,10 @@ function findMax_R(array, index){
    
 }
 
+function getRandomInt(min,max){
+	return Math.floor(Math.random()*(max-min+1))+min; //min and max inclusive
+}
+
 function calcVectorMag(x1,y1,x2,y2) {
 	var xDis = x2-x1;
 	var yDis = y2-y1;
