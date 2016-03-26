23bc0e21bced893727565acbdaa013d11232000e
sjdodge123 <sjdodge@outlook.com>
2016-03-26 14:13:59 -0400

    generalized deployment files for distribution on other computers

--- deploy.bat
+++ deploy.bat
@@ -1,3 +1,3 @@
 
 winscp /console /script=.\copy.txt
-plink -ssh -batch -i "C:\Users\sdodge\Desktop\nodedev\nodeprivatekey.ppk" -m .\restart.txt sdodge@node.dev
+plink -ssh -batch -i deployment\nodeprivatekey.ppk -m .\restart.txt sdodge@node.dev
--- deployDebug.bat
+++ deployDebug.bat
@@ -1,6 +1,6 @@
 
 winscp /console /script=.\copy.txt
-plink -ssh -batch -i "C:\Users\sdodge\Desktop\nodedev\nodeprivatekey.ppk" -m .\restart.txt sdodge@node.dev
+plink -ssh -batch -i "%~dp0\deployment\nodeprivatekey.ppk" -m .\restart.txt sdodge@node.dev
 
 start "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" http://node.dev:8080/debug?port=5858
 
